d18805198353cccde3820e68f95b5782db004417
gocchi <shogo@gocchi.localdomain>
2026-01-03 22:00:04 +0900

    クラス完成（仮）

--- mypkg/node_1kari.py
+++ mypkg/node_1kari.py
@@ -5,12 +5,12 @@ from std_msgs.msg import Float32
 from geometry_msgs.msg import Point
 import numpy as np
 
-class Inkinematics():
+class Inkinematics(Node):
     def __init__ (self):
         self.node = Node("node_1kari")
         self.sub_dt = self.node.create_subscription(Float32,"delta_t",self.get_dt,1)
         self.sub_dt = self.node.create_subscription(Float32,"wheel_dist",self.get_l,1)
-        self.sub_coords = self.node.create_subscription(Point,"coordinates",10)
+        self.sub_coords = self.node.create_subscription(Point,"coordinates",self.cb,10)
         self.pub = self.node.create_publisher(Float32MultiArray,"velocities",10)
         self.n=0
         
@@ -18,7 +18,7 @@ class Inkinematics():
         self.data_p=[0,0]
         self.delection_n=0
         self.delection_p=0
-        self.vc=0
+        self.c=0
         
         self.dt
         self.l
@@ -43,9 +43,23 @@ class Inkinematics():
         self.data_n.append(msg.x)
         self.data_n.append(msg.y)
         
+        self.v = np.sprt((self.data_n[0]-self.data_p[0])**2/self.dt+(self.data_n[1]-self.data_p[1])**2/self.dt)
+        
         self.delection_n = np.atan2(self.data_n[0]-self.data_p[0],self.data_n[1]-self.data_p[1])
         
+        if self.delection_n < (-np.pi/2) and (np.pi/2)<self.delection_p:
+            self.delection_p=-2*np.pi+self.delection_p
+        if self.delection_p < (-np.pi/2) and (np.pi/2)<self.delection_n:
+            self.derection_p=2*np.pi+self.derection_p
+            
         self.omega = (self.delection_n-self.delection_p)/self.dt
+        
+        bect = np.array([self.omega,self.v])
+        ans = np.linalg.solve(self.matrix, bect)
+        
+        outputs = Float32MultiArray()
+        outputs.data = [float(ans[0]), float(ans[1])]
+        self.pub.publish(outputs)
         
         
         
