1a5daf20a625b813a579831279bb7b90912aeaac
gocchi <shogo@gocchi.localdomain>
2026-01-04 14:53:13 +0900

    テスト用ノード追加

---
+++ mypkg/test1node.py
@@ -0,0 +1,9 @@
+# Copyright 2025 Shogo Takizawa
+# SPDX-License-Identifier: BSD-3-Clause
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Float32
+from geometry_msgs.msg import Point
+
