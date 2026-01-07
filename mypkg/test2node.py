f590cf5e5df24cf82b482e7138e23f0f7758bd1c
gocchi <shogo@gocchi.localdomain>
2026-01-07 19:46:40 +0900

    test2追加

---
+++ mypkg/test2node.py
@@ -0,0 +1,34 @@
+# Copyright 2025 Shogo Takizawa
+# SPDX-License-Identifier: BSD-3-Clause
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Point
+import numpy as np
+
+class Test(Node):
+    def __init__(self):
+        super().__init__("test1node")
+        self.pub=self.create_publisher(Point,"coordinates",10)
+        self.l=150
+        self.dt=0.5
+        self.n=0
+        
+        self.create_timer(self.dt,self.cb)
+        
+    def cb(self):
+        x=np.cos(np.pi*0.05*self.n-np.pi/2)*1000
+        y=1000+np.sin(np.pi*0.05*self.n-np.pi/2)*1000
+        out=Point()
+        out.x=x
+        out.y=y
+        self.pub.publish(out)
+        self.n+=1
+        
+        
+def main():
+    rclpy.init()
+    node=Test()
+    rclpy.spin(node)
+        
+
