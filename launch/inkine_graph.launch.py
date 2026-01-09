0392c3609175fe3939e07180c8ada79d07892dfb
gocchi <shogo@gocchi.localdomain>
2026-01-09 18:01:55 +0900

    launch仮完成

---
+++ launch/inkine_graph.launch.py
@@ -0,0 +1,29 @@
+# Copyright 2026 Shogo Takizawa
+# SPDX-License-Identifier: BSD-3-Clause
+
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+
+
+def generate_launch_description():
+    
+    para=[{
+                'delta_t':0.50,
+                'wheel_dist':150.0,
+            }] 
+    
+    inkinematics = launch_ros.actions.Node(
+        package='mypkg',      
+        executable='inkinematics',  
+        parameters=para
+        )
+    
+    graph_v = launch_ros.actions.Node(
+        package='mypkg',      
+        executable='graph_v', 
+        parameters=para
+        )
+    
+    return launch.LaunchDescription([inkinematics,graph_v])   
